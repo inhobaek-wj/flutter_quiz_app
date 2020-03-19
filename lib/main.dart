d97999aa304bfdcc7c57e5a1e9ef98d5412d7229
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-19 20:32:26 +0900

    change StatelessWidget -> StatefulWidget using State<> & setState()

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// StatelessWidget can't tell Flutter that it should re-run build() upon such changes.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() { // connection to State.
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyApp> { // State is generic, so it needs class pointer. Connection fo Widget.
+
+  var questions = [
+    'What is yout favorite color?',
+    'What is your favorite animal?'
+  ];
+  int questionIndex = 0;
 
   // this method is automatically called by flutter.
   // it returns a so-colled "widget-tree" which tells Flutter what to draw onto the screen.
   @override
   Widget build(BuildContext context) { // Widget is the core UI building block.
 
-    void answerQuetion() => print('Answer chosen!');
+    void answerQuetion() {
+      //setState() is a "trigger" that informs Flutter that it needs to re-run build() of the Widget.
+      setState(() {
+          questionIndex = questionIndex + 1;
+      });
+      print(questionIndex);
+    }
 
     // home is core widget which flutter brings out of the screen, and it's named arg.
     return MaterialApp(home: Scaffold(
         appBar: AppBar(title: Text('My First App'),),
         body: Column(children: <Widget>[
-            Text('The Questin!'),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuetion),
             RaisedButton(child: Text('Answer 2'), onPressed: answerQuetion),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuetion),
