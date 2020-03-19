a534eea5ab1d2b0f4617a8a8c584f5fa72900aa6
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-19 17:05:32 +0900

    apply arrow function to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp()); // runApp builds the main widget and calls build() on it.
-}
+// void main() {
+//   runApp(MyApp()); // runApp builds the main widget and calls build() on it.
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
