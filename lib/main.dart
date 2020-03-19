e0c08985af516ca9b345b1329cbd9f1aa9e0c55f
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-19 16:54:47 +0900

    add comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // runApp builds the main widget and calls build() on it.
 }
 
 class MyApp extends StatelessWidget {
 
+  // this method is automatically called by flutter.
+  // it returns a so-colled "widget-tree" which tells Flutter what to draw onto the screen.
   @override
-  Widget build(BuildContext context) {
-    // home is core widget which flutter brings out of the screen.
+  Widget build(BuildContext context) { // Widget is the core UI building block.
+
+    // home is core widget which flutter brings out of the screen, and it's named arg.
     return MaterialApp(home: Text('Hello, Dart!'),);
   }
 
